1515830f0e61bfb53a90f458c4928e65df255e60
irfanbaig7 <ibaig9356@gmail.com>
2025-04-28 09:53:25 +0530

    Some modification added

--- 02counterproject/src/App.jsx
+++ 02counterproject/src/App.jsx
@@ -1,50 +1,47 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
+import { useState } from 'react'
+import './App.css'
 
-// function App() {
+function App() {
 
-//   const [counter, setCounter] =  useState(15) // this are a hook
+  const [counter, setCounter] =  useState(15) // this are a hook
 
-//   const addValue = () => {
-//       // if (counter < 20) {
-//       //   setCounter(counter + 1)
-//       // }
+  const addValue = () => {
+      if (counter < 20) {
+        setCounter(counter + 1)
+      }
 
-//       // Bundling all increament like this at One timed.
 
-//       // setCounter(prevCounter => prevCounter + 1)
-//       // setCounter(prevCounter => prevCounter + 1)
-//       // setCounter(prevCounter => prevCounter + 1)
-//       // setCounter(prevCounter => prevCounter + 1)
-//   }
+      setCounter(prevCounter => prevCounter + 1)
+      setCounter(prevCounter => prevCounter + 1)
+      setCounter(prevCounter => prevCounter + 1)
+      setCounter(prevCounter => prevCounter + 1)
+  }
 
-//   // const removeValue = () => {
-//   //   // if (counter > 0) {
-//   //   //   setCounter(counter - 1)
-//   //   // }
+  const removeValue = () => {
+    if (counter > 0) {
+      setCounter(counter - 1)
+    }
 
-//   //   setCounter(counter - 1)
-//   // }
+    setCounter(counter - 1)
+  }
 
-//   return (
-//     <>
-//       <h1>Chai aur react</h1>
-//       <h2>Counter Value : {counter}</h2>
-//       <button
-//       onClick={addValue}
-//       >Add Value {counter}</button>
-//       <br />
-//       <button
-//         onClick={removeValue}
-//       >Remove Value {counter}</button>
-//       <p>Foooter: {counter}</p>
-//     </>
-//   )
-// }
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter Value : {counter}</h2>
+      <button
+      onClick={addValue}
+      >Add Value {counter}</button>
+      <br />
+      <button
+        onClick={removeValue}
+      >Remove Value {counter}</button>
+      <p>Foooter: {counter}</p>
+    </>
+  )
+}
 
-// export default App
+export default App
 
 
 
