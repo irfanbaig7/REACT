31d5faf8d77c92b83d517983d57be45ee1aa5867
irfanbaig7 <ibaig9356@gmail.com>
2025-04-28 09:56:07 +0530

    Some modification added

--- 08Contextmini/src/context/UserContxtProvider.jsx
+++ 08Contextmini/src/context/UserContxtProvider.jsx
@@ -1,12 +1,19 @@
-import React from "react";
-import userContext from "./userContext";
-const UserContextProvider = ({ children }) => {
-  const [user, setUser] = React.useState(null);
-  return (
-    <userContext.Provider value={{ user, setUser }}>
-      {children}
-    </userContext.Provider>
-  );
-};
 
-export default UserContextProvider;
+
+// why children on there and what exactliy work on them
+
+import React, { useState } from 'react'
+import userContext from '../context/userContext'
+
+export default function UserContxtProvider({children}) {
+
+  // Here is give some data!
+  const [user, setUser] = useState(null)
+
+  return (
+    // here is we give access like props ex user, setuser like this this goan be passing Object.
+    <userContext.Provider value={{user, setUser}}>
+      {children}
+    </userContext.Provider>   
+  )
+}
