45b94043733bbdc57ead7e9481da9ec721865eb6
irfanbaig7 <ibaig9356@gmail.com>
2025-04-28 09:56:22 +0530

    Some modification added

--- 08Contextmini/src/context/userContext.js
+++ 08Contextmini/src/context/userContext.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React from 'react'
 
-const userContext = React.createContext()
+const UserContext = React.createContext()
 
-export default userContext;
+export default UserContext;
 
