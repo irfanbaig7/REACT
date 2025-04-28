8fb69e865dff8593b12f00653148a69e41d45e62
irfanbaig7 <ibaig9356@gmail.com>
2025-04-28 09:55:23 +0530

    Some modification added

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useState } from "react";
 
 const App = () => {
@@ -6,16 +7,38 @@ const App = () => {
   return (
     <div
       className="flex justify-center h-screen w-full items-end"
-      style={{ backgroundColor: color }}
-    >
+      style={{ backgroundColor: color, transition: "background-color 0.2s ease-in-out" }}>
       <div className="h-20 w-[80%] flex items-center justify-center gap-3 border bg-gray-800 rounded-xl mb-40">
-        <button onClick={() => setColor("Blue")}  className="p-3 text-white border-none rounded-lg" style={{backgroundColor: "blue"}}>blue</button>
-        <button onClick={() => setColor("green")} className="p-3 border-none  rounded-lg text-white" style={{backgroundColor: "green"}}>
+        <button
+          onClick={() => setColor("Blue")}
+          className="p-3 text-white border-none rounded-lg"
+          style={{ backgroundColor: "blue" }}>
+          blue
+        </button>
+        <button
+          onClick={() => setColor("green")}
+          className="p-3 border-none  rounded-lg text-white"
+          style={{ backgroundColor: "green" }}>
           green
         </button>
-        <button onClick={() => setColor("Yellow")} className="p-3 border rounded-md " style={{backgroundColor: "yellow"}}>Yellow</button>
-        <button onClick={() => setColor("Pink")} className="p-3 border rounded-md " style={{backgroundColor: "pink"}}>Pink</button>
-        <button onClick={() => setColor("red")} className="p-3 border rounded-md text-white" style={{backgroundColor: "red"}}>Red</button>
+        <button
+          onClick={() => setColor("Yellow")}
+          className="p-3 border rounded-md "
+          style={{ backgroundColor: "yellow" }}>
+          Yellow
+        </button>
+        <button
+          onClick={() => setColor("Pink")}
+          className="p-3 border rounded-md "
+          style={{ backgroundColor: "pink" }}>
+          Pink
+        </button>
+        <button
+          onClick={() => setColor("red")}
+          className="p-3 border rounded-md text-white"
+          style={{ backgroundColor: "red" }}>
+          Red
+        </button>
       </div>
     </div>
   );
