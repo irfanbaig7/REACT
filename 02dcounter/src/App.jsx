b5a7d2e902593539837cc13a9344d23f87aa51bf
irfanbaig7 <ibaig9356@gmail.com>
2025-04-28 09:54:46 +0530

    Some modification added

--- 02dcounter/src/App.jsx
+++ 02dcounter/src/App.jsx
@@ -1,59 +1,47 @@
 import React from "react";
 import { useState } from "react";
 
-
 const App = () => {
 
-  // For Counter
-  // let [counter, setCounter] = useState(10);
+  let [counter, setCounter] = useState(0);
 
-  // const increase = () => {
-  //   if (counter >= 0 && counter < 20) {
-  //     counter = counter + 1;
-  //   }
+  const increase = () => {
+    if (counter >= 0) {
+      counter++;
+    }
 
-  //   setCounter(counter);
-  //   console.log(counter);
-  // };
-  // const decrease = () => {
-  //   if (counter > 0) {
-  //     counter = counter - 1;
-  //   } else {
-  //     counter = 0; 
-  //   }
-  //   setCounter(counter);
-  //   console.log(counter);
-  // };
+    setCounter(counter);
+  };
 
-
-
-  // For Courrency
-
+  const decrease = () => {
+    if (counter > 0) {
+      counter--;
+    } else {
+      counter = 0;
+    }
+    setCounter(counter);
+  };
 
 
   return (
 
-    // For Counter
-
-    // <div>
-    //   <h1 className="text-yellow-500 text-center mb-3 text-2xl">Counter : {counter}</h1>
-    //   <button  onClick={increase} className="py-2 border-sky-500 border px-2 rounded-lg mr-3">Increase</button>
-    //   <button onClick={decrease} className="py-2 border-sky-500 border px-2 rounded-lg">Decrease</button>
-    // </div>
-
-
-
-    // For Currecny
-
     <div>
-      <h1>Irfan baig</h1>
+      <h1 className="text-yellow-500 text-center mb-3 text-2xl">
+        Counter : {counter}
+      </h1>
+      <button
+        onClick={increase}
+        className="py-2 border-sky-500 border px-2 rounded-lg mr-3">
+        Increase
+      </button>
+      <button
+        onClick={decrease}
+        className="py-2 border-sky-500 border px-2 rounded-lg">
+        Decrease
+      </button>
     </div>
-
 
   );
 };
 
 export default App;
-
-
-
